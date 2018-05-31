3d2faeb954fce94eef97f17b33dfaab95d70a87a
LymarV <lymar.vm@gmail.com>
2018-05-31 16:10:06 +0300

    Create TaskWrapper extensions for a view model

---
+++ XamarinToolkit/Extensions/ViewModelExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Diversido.XamarinToolkit.ViewModels.Abstraction;
+
+namespace Diversido.XamarinToolkit.Extensions
+{
+	public static class ViewModelExtensions
+	{
+		internal static TaskWrapper WrapTask (this Task task, IViewModelBase vm)
+		{
+			return new TaskWrapper (vm, task);
+		}
+
+		internal static TaskWrapper WrapTaskDefault (this Task task, IViewModelBase vm)
+		{
+			return new TaskWrapper (vm, task).WrapDefault ();
+		}
+
+		internal static TaskWrapper WrapTaskWithSelectionLoading (this Task task, IViewModelBase vm)
+		{
+			return new TaskWrapper (vm, task).WrapWithSelectionLoading ();
+		}
+
+		internal static TaskWrapper WrapTaskWithSilentLoading (this Task task, IViewModelBase vm)
+		{
+			return new TaskWrapper (vm, task).WrapWithSilentLoading ();
+		}
+	}
+}
