c2e8ea5b783af081d54989bf04702f94a98d98e2
LymarV <lymar.vm@gmail.com>
2018-05-31 16:07:55 +0300

    Create an interface for base view model

---
+++ XamarinToolkit/ViewModels/Abstraction/IViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Diversido.XamarinToolkit.ViewModels.Abstraction
+{
+	public interface IViewModelBase
+	{
+		bool IsLoading { get; set; }
+		bool IsSilentLoading { get; set; }
+		bool IsSelectionLoading { get; set; }
+
+		Task WrapWithConnectivityCheck (Task task, bool throwOnFail);
+		Task WrapWithExceptionHandling (Task task);
+
+		Task WrapWithLoading (Task task);
+		Task WrapWithSilentLoading (Task task);
+		Task WrapWithSelectionLoading (Task task);
+	}
+}
