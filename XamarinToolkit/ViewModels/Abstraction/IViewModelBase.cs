c8f628f9691a90bb7a7848a2d28b0a86f61fb5bd
LymarV <lymar.vm@gmail.com>
2018-06-07 11:49:11 +0300

    Clear IViewModelBase

--- XamarinToolkit/ViewModels/Abstraction/IViewModelBase.cs
+++ XamarinToolkit/ViewModels/Abstraction/IViewModelBase.cs
@@ -1,14 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace Diversido.Toolkit.ViewModels.Abstraction
+﻿namespace Diversido.Toolkit.ViewModels.Abstraction
 {
 	public interface IViewModelBase
 	{
-		bool IsBusy { get; set; }
-
-		Task WrapWithConnectivityCheck (Task task, bool throwOnFail);
-		Task WrapWithExceptionHandling (Task task);
-
-		Task WrapWithLoading (Task task);
+		
 	}
 }
